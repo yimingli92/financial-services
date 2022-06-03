d94b0b7dd40524618c8d46fd2900dcf2cb054eed
shivpatel426 <70605497+shivpatel426@users.noreply.github.com>
2022-06-03 11:33:38 -0400

    Add files via upload

--- handler.py
+++ handler.py
@@ -8,9 +8,6 @@ from datetime import date, datetime
 # 2. (If using lambda) Write your code, then select all files and zip into function.zip
 # a) Mac/Linux --> zip -r9 ${PWD}/function.zip
 
-# Lambda Permissions:
-# AWSLambdaVPCAccessExecutionRole
-# remove info
 
 # Configuration
 database_endpoint = ''
@@ -44,6 +41,7 @@ def get_path_ids(path):
 
 
 def execute_statement(query, param):
+    global cursor
     cursor = connection.cursor()
     query_statement = query.format(param)
     print(query_statement)
@@ -73,6 +71,23 @@ def create_account_record(record):
     response['balance'] = record[3]
     response['createdDate'] = record[4]
     response['active'] = record[5]
+    return response
+    
+def create_customer_record(record):
+    response = {}
+    response['customerId'] = record[0]
+    response['firstName'] = record[1]
+    response['lastName'] = record[2]
+    response['businessName'] = record[3]
+    response['phone'] = record[4]
+    response['email'] = record[5]
+    response['addressLine1'] = record[6]
+    response['addressLine2'] = record[7]
+    response['city'] = record[8]
+    response['state'] = record[9]
+    response['country'] = record[10]
+    response['customerGrouping'] = record[11]
+    response['owner'] = record[12]
     return response
 
 
@@ -113,21 +128,132 @@ def create_http_response(code, records):
     return http_response
 
 
-def process_transactions(method, path):
-    response = {}
-    ids = get_path_ids(path)
-    if not ids:
-        return response
-
+def process_transactions(method, path, event):
     all_records = []
+    
     if method == 'GET':
+        response = {}
+        ids = get_path_ids(path)
+        if not ids:
+            return response
+            
         records = execute_statement('SELECT * from Transaction where transactionId in ({})', ids)
         for record in records:
             all_records.append(create_transaction_record(record))
+            
+    elif method == 'POST':
+        # data = json.loads(event['body'])['transactionId']                                             ####Test code
+        # records = execute_statement('SELECT * from Transaction where transactionId in ({})', data)    ####Test code
+        # for record in records:                                                                        ####Test code
+        #     all_records.append(create_transaction_record(record))                                     ####Test code
+        
+        cols = ''
+        vals = ''
+        data = json.loads(event['body'])
 
+        for key, value in data.items(): ### NEED TO BUILD IN HANDLING, IF WE RECEIVE MULTIPLE TRANSACTIONS IN SINGLE PAY-LOAD
+            cols += key + ','
+            vals += value + ','
+            
+        insert_statement = "INSERT INTO Transaction VALUES ({})"
+        
+        try:
+            execute_statement(insert_statement, vals[:-1])
+            print("\nINSERT SUCCESSFUL...COMMITING CHANGES...\n")
+            connection.commit()
+
+            print("Number of records inserted: ", cursor.rowcount)
+            print("TransactionID of inserted record:", cursor.lastrowid)
+            last_transaction = cursor.lastrowid
+            
+            print("\nChecking customers balance to validate transaction...\n")
+            balance_check_statment = "SELECT t.transactionId, t.amount, t.creditOrDebit, t.customerId, t.createdDate, t.status, t.completedDate, t.cancelledDate, t.rejectedDate, c.customerGrouping, " \
+                                    "c.firstName, c.lastName, c.businessName, a.accountNumber, a.balance FROM Transaction t " \
+                                    "INNER JOIN Customer c ON c.customerId = t.customerId " \
+                                    "INNER JOIN Account a ON a.customerId = t.customerId " \
+                                    "WHERE t.transactionId = {}"
+
+            results = execute_statement(balance_check_statment, last_transaction)
+
+            for result in results:           
+                print(f"\nCurrent Account balance: {result[-1]}")   
+
+                if ((result[-1] > result[1]) & (result[2] == 1)) | (result[2] == 0):
+
+                    try:
+                         ### UPDATING TRANSACTION ###
+                        update_transaction = "UPDATE Transaction " \
+                                    f"SET completedDate = '{datetime.now()}', status = 'COMPLETED' " \
+                                    "WHERE transactionId = {}" 
+
+                        print("\nUpdating transaction...")
+                        execute_statement(update_transaction, last_transaction)
+                        print("\nNumber of records updated: ", cursor.rowcount)
+                        print("\nTransactionID of updated record:", last_transaction)
+
+                        ### UPDATING ACCOUNT BALANCE ###
+
+                        if result[2] == 1: #CREDIT TRANSACTION
+                            print(f"""\nCustomer has enough balance for transaction #{last_transaction} for the amt of {result[1]}...
+                                      \nUpdating account balance...""")
+
+                            transaction_type = "Credit" 
+                            new_balance = result[-1] - result[1]
+
+                        elif result[2] == 0: #DEBIT TRANSACTION
+                            transaction_type = "Debit"
+                            new_balance = result[-1] + result[1]
+
+                        update_balance = "UPDATE Account "\
+                            f"SET balance = {new_balance} "\
+                            "WHERE accountNumber = {}"
+
+                        account_num = result[-2]
+
+                        print(f'\n{transaction_type}ing Account balance...')
+                        execute_statement(update_balance, account_num)
+                        print("\nNumber of records updated: ", cursor.rowcount)
+                        print("\nAccountID of updated record:", account_num)        
+                        print(f"\nCustomer new balance: {new_balance}\n")
+
+                        connection.commit()
+
+                    except Exception as e:
+                        print("\nError while making Transaction/Account updates...ROLLING BACK CHANGES...")
+                        print("\nError: \n", e)
+                        connection.rollback()                  
+
+                else:
+                    print(f"\nCustomer has insufficient funds for transaction #{last_transaction}...\nRejecting transaction...")
+
+                    update_transaction = "UPDATE Transaction "\
+                                            f"SET rejectedDate = '{datetime.now()}', status = 'REJECTED' " \
+                                            "WHERE transactionId = {}"
+
+                    try:
+                        print("\nUpdating transaction...")
+                        execute_statement(update_transaction, last_transaction)
+                        print("\nNumber of records updated: ", cursor.rowcount)
+                        print("\nTransactionID of updated record:", last_transaction)
+                        connection.commit()
+
+                    except Exception as e:
+                        print("\nUpdating transaction FAILED...ROLLING BACK CHANGES...")
+                        print("\nError: ", e)  
+                        connection.rollback() 
+                        
+            records = execute_statement('SELECT * from Transaction where transactionId in ({})', last_transaction)
+            for record in records:
+                all_records.append(create_transaction_record(record))
+
+        except Exception as e:
+            print("\nINSERT FAILED...ROLLING BACK CHANGES...\n")
+            print("Error: ", e)
+            connection.rollback()      
+        
     http_response = create_http_response(200, all_records)
     return http_response
-
+    
 
 def process_transaction_info(method, path, query_params):
     response = {}
@@ -152,25 +278,93 @@ def process_transaction_info(method, path, query_params):
     return http_response
 
 
-def process_accounts(method, path):
-    response = {}
-    ids = get_path_ids(path)
-    if not ids:
-        return response
-
+def process_accounts(method, path, event):
     all_records = []
+
     if method == 'GET':
+        response = {}
+        ids = get_path_ids(path)
+        if not ids:
+            return response
+        
         records = execute_statement('SELECT * from Account where accountNumber in ({})', ids)
         for record in records:
-            all_records.append(create_account_record(record))
+            all_records.append(create_account_record(record))            
+    
+    elif method == 'POST':
+        cols = ''
+        vals = ''
+        data = json.loads(event['body'])
+
+        for key, value in data.items():
+            cols += key + ','
+            vals += value + ','
+
+        insert_statement = "INSERT INTO Account VALUES ({})"
+
+        try:
+            execute_statement(insert_statement, vals[:-1])
+            print("\nINSERT SUCCESSFUL...COMMITING CHANGES...\n")
+            connection.commit()
+
+            print("Number of records inserted: ", cursor.rowcount)
+            print("Account number of inserted record:", cursor.lastrowid)
+            last_account = cursor.lastrowid
+            records = execute_statement('SELECT * from Account where accountNumber in ({})', last_account)
+            for record in records:
+                all_records.append(create_account_record(record))
+            
+        except Exception as e:
+            print("\nINSERT FAILED...ROLLING BACK CHANGES...\n")
+            print("Error: ", e)
+            connection.rollback()      
 
     http_response = create_http_response(200, all_records)
     return http_response
 
 
-def process_customers(method, path):
-    response = {}
-    http_response = create_http_response(404, [])
+def process_customers(method, path, event):  
+    all_records = []
+    
+    if method == 'GET':
+        response = {}
+        ids = get_path_ids(path)
+        if not ids:
+            return response
+        
+        records = execute_statement('SELECT * FROM Customer where customerID in ({})', ids)
+        for record in records:
+            all_records.append(create_customer_record(record))
+            
+    elif method == 'POST':
+        cols = ''
+        vals = ''
+        data = json.loads(event['body'])
+
+        for key, value in data.items():
+            cols += key + ','
+            vals += value + ','
+
+        insert_statement = "INSERT INTO Customer VALUES ({})"
+
+        try:
+            execute_statement(insert_statement, vals[:-1])
+            print("\nINSERT SUCCESSFUL...COMMITING CHANGES...\n")
+            connection.commit()
+
+            print("Number of records inserted: ", cursor.rowcount)
+            print("CustomerID of inserted record:", cursor.lastrowid)
+            last_customer = cursor.lastrowid
+            records = execute_statement('SELECT * from Customer where customerID in ({})', last_customer)
+            for record in records:
+                all_records.append(create_customer_record(record))
+
+        except Exception as e:
+            print("\nINSERT FAILED...ROLLING BACK CHANGES...\n")
+            print("Error: ", e)
+            connection.rollback()  
+    
+    http_response = create_http_response(200, all_records)
     return http_response
 
 
@@ -193,11 +387,11 @@ def lambda_handler(event, context):
     response = {}
     print('root of path: ' + str(root))
     if root == 'transactions':
-        response = process_transactions(method, path)
+        response = process_transactions(method, path, event)
     elif root == 'accounts':
-        response = process_accounts(method, path)
+        response = process_accounts(method, path, event)
     elif root == 'customers':
-        response = process_customers(method, path)
+        response = process_customers(method, path, event)
     elif root == 'transactioninfo':
         response = process_transaction_info(method, path, query_params)
     else:
